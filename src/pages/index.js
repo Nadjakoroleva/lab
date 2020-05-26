1c5170da67b0666a51187901e25bf7d983e15b24
Nadjakoroleva <nadin.koroleva@gmail.com>
2020-05-26 09:10:59 +0300

    init

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState, useRef } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+
+import { GlobalStyle } from '../components/globalStyle';
+import '../components/fonts.css';
+
+const IndexPage = () => {
+  return <>ksks</>;
+};
+
+export default IndexPage;
